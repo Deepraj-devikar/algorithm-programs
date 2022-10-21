1818c88226bf94cca92a935ab0708cd88a202ff3
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-10-21 18:39:17 +0530

    ADDED: Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,48 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InsertionSort {
+
+	public void insertAndSort(String[] words, String word, int i) {
+		while(i > 0) {
+			if(word.compareToIgnoreCase(words[i-1]) < 0) {
+				words[i] = words[--i];
+				continue;
+			}
+			words[i] = word;
+			return;
+		}
+		words[i] = word;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to insertion sort program");
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter number how many words have to enter : ");
+		int numberOfWords;
+		while(true) {
+			try {
+				numberOfWords = scanner.nextInt();
+				break;
+			}catch(InputMismatchException e) {
+				System.out.print("Please enter number : ");
+			}finally {
+				scanner.nextLine();
+			}
+		}
+
+		InsertionSort insertionSort = new InsertionSort();
+		String words[] = new String[numberOfWords];
+		for(int i = 0; i < numberOfWords; i++) {
+			String word = scanner.nextLine().trim();
+			insertionSort.insertAndSort(words, word, i);
+		}
+		scanner.close();
+		System.out.println("**********************************");
+		System.out.println("Sorted array : ");
+		for(int i = 0; i < numberOfWords; i++) {
+			System.out.println(words[i]);
+		}
+	}
+}
